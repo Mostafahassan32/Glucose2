33610585b14432149f4e473f0ae5fb2ee826b8eb
Mostafahassan32 <168636933+Mostafahassan32@users.noreply.github.com>
2024-05-15 19:18:15 +0300

    Add files via upload

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -3,7 +3,19 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("Task3.controller.View1", {
+	return Controller.extend("Task.controller.View1", {
+		onInit: function() {
+  var oModel = new sap.ui.model.odata.v2.ODataModel("https://services.odata.org/V4/Northwind/Northwind.svc/");
+ this.getView().setModel(oModel, "employee");
+ //sap.ui.getcore().setModel(oModel, "employee");
+ 
 
+
+
+
+    
+    
+		
+		}
 	});
 });
